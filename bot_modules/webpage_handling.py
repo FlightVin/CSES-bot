4698dde286f7285a3ae71a9d9329bdf9307c41db
Vineeth Bhat <vineeth.bhat@gmail.com>
2022-12-20 22:21:30 +0530

    Implement login

--- bot_modules/webpage_handling.py
+++ bot_modules/webpage_handling.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 CSES_BASE_URL = 'https://cses.fi/'
 CSES_LOGIN_URL = CSES_BASE_URL + 'login'
@@ -11,4 +12,22 @@ class webpage(webdriver.Chrome):
     def login(self):
         self.get(CSES_LOGIN_URL)
 
+        with open("./secret/user-key") as fobj:
+            self.username = fobj.readline().strip()
+            self.password = fobj.readline().strip() 
         
+        # Fill in username
+        username_box = self.find_element(by=By.ID, value='nick')
+        username_box.clear()
+        username_box.send_keys(self.username)
+
+        # FIll in password
+        password_box = self.find_element(by=By.NAME, value='pass')
+        password_box.clear()
+        password_box.send_keys(self.password)
+    
+        # Click on submit button
+        self.find_element(by='xpath', value="//input[@type='submit']").click()
+
+    def quit(self):
+        self.quit()
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import bot_modules.webpage_handling as webp
 
 if __name__ == '__main__':
+
     print("Starting bot...")
 
     current_session = webp.webpage()
